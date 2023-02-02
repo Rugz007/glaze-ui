8776fe9ba99fe4c8e57f2150275f8665301bb4e4
Rushil Somwanshi <rushil@bitglaze.com>
2023-02-03 00:27:45 +0530

    style(prettier): format code

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,32 +1,29 @@
 import { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
     {
-      name: "@storybook/addon-postcss",
+      name: '@storybook/addon-postcss',
       options: {
         postcssLoaderOptions: {
-          implementation: require("postcss"),
+          implementation: require('postcss'),
         },
       },
     },
   ],
-  "framework": {
-    "name": "@storybook/nextjs",
-    "options": {}
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
   },
-  "core": {
-    "disableTelemetry": true
+  core: {
+    disableTelemetry: true,
   },
-  "docs": {
-    "autodocs": "tag"
-  }
+  docs: {
+    autodocs: 'tag',
+  },
 };
 export default config;
