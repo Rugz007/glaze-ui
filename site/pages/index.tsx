8e06193e743eafa8610689bf7a214f89680f0c20
Rugz007 <dsweb007@gmail.com>
2023-04-09 21:15:41 +0530

    feat(site): basic homepage added

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,18 +1,19 @@
+import { Button } from '@bitglaze/glaze-ui';
+
 export default function Home() {
   return (
     <main className="grid w-full h-screen grid-cols-12 bg-slate-100">
       <div className="flex flex-col items-center justify-center w-full col-span-6">
-        <img
-          src="logo.svg"
-          alt="logo"
-          style={{ height: '10%', paddingBottom: 0 }}
-        />
-        <h1 className="w-full mb-6 font-medium text-center text-8xl">
-          {' '}
-          glaze ui
-        </h1>
-        <h4 className="text-3xl">Simple. Flexible. Performant. Remarkable.</h4>
-        <button>Github</button>
+        <div className="flex justify-center mb-0 h-fit ">
+          <img src="logo.svg" alt="logo" className="mb-0 mr-4 h-28" />
+          <h1 className="w-full font-medium text-center h-fit text-8xl">
+            glaze-ui
+          </h1>
+        </div>
+        <h4 className="my-4 text-3xl">
+          Simple. Flexible. Performant. Remarkable.
+        </h4>
+        <Button className="bg-primary-700">Github</Button>
       </div>
       <div> </div>
     </main>
