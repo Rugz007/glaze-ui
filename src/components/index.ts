cfc313955e5d858793336d18e090e942333bd9a6
Rugz007 <dsweb007@gmail.com>
2023-04-14 00:22:38 +0530

    fix(index):  Updated index.ts for getting all components

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,8 @@ export * from './Button/Button';
 export * from './Checkbox/Checkbox';
 export * from './Dialog/Dialog';
 export * from './Dropdown/Dropdown';
+export * from './Input/Input';
+export * from './Tag/Tag';
+export * from './Switch/Switch';
+export * from './Select/Select';
+export * from './Avatar/Avatar';
