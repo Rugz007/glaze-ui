b2c6ca83f8d9dbd2debd269de8551afea10c2f11
khaziashwaq <ashwaqkhazi1729@gmail.com>
2023-04-18 18:03:36 +0530

    feat(component): tooltip component added

---
+++ src/components/ToolTip/ToolTip.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from './ToolTip';
+import { Button } from '../Button/Button';
+import { Plus } from 'lucide-react';
+
+const meta: Meta<typeof Tooltip> = {
+  title: 'Tooltip',
+  component: Tooltip,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Tooltip>;
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant="outline" className="absolute top-1/2 left-1/2">
+              Add
+            </Button>
+          </TooltipTrigger>
+
+          <TooltipContent>
+            <p>Add to library</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    );
+  },
+};
