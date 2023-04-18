abd893461a298b83f867d16025b89379d77fbb36
khaziashwaq <ashwaqkhazi1729@gmail.com>
2023-04-18 18:12:47 +0530

    refactor(component): removed unused imports from textarea component

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -1,6 +1,5 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import Toaster from '../../src/components/Toast/Toaster';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
